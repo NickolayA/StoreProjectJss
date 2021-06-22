fc9d41779f761b1115ad362d79a3ae6587a64aa6
m.akhramovich <m.akhramovich@reply.com>
2021-06-22 22:09:45 +0300

    all components are moved to ts language

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Placeholder } from '@sitecore-jss/sitecore-jss-react';
 
-export const Footer = (props) => {
+export const Footer = (props: any) => {
   return (
     <div>
       <Placeholder name="footer" rendering={props.rendering} />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Placeholder } from '@sitecore-jss/sitecore-jss-react';
 
-export const Header = (props) => {
+export const Header = (props: any) => {
   return (
     <div>
       <Placeholder name="header" rendering={props.rendering} />
