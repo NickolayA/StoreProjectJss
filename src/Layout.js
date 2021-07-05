f810f88a9cef1de0a791517d78dc6220165e4fff
m.akhramovich <m.akhramovich@reply.com>
2021-07-05 23:57:30 +0300

    Styles are added

--- src/Layout.js
+++ src/Layout.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Placeholder } from '@sitecore-jss/sitecore-jss-react';
 
+import './assets/css/animate.css';
+import './assets/css/owl.carousel.min.css';
+import './assets/css/owl.theme.default.min.css';
+import './assets/css/magnific-popup.css';
+import './assets/css/flaticon.css';
+import './assets/css/style.css';
+
 const Layout = ({ route }) => (
-  <React.Fragment>
-    <div className="container">
-      <Placeholder name="jss-main" rendering={route} />
-    </div>
-  </React.Fragment>
+	<React.Fragment>
+		<div className="wrap">
+			<Placeholder name="jss-main" rendering={route} />
+		</div>
+	</React.Fragment>
 );
 
 export default Layout;
